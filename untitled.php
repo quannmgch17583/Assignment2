8da9e063dd235294122390fe87f157d6460b4cae
quannmgch17583 <quannmgch17583@fpt.edu.vn>
2019-11-04 01:26:02 +0700

    Test Heroku + Connection

---
+++ untitled.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>This is the fking testing for the fking assignment</h1>
+</body>
+</html>
